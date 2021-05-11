40dc5aa71f089b30df323cb1d9464f3a4cb4a554
Thirdy Fetalvero <botsot.felix@gmail.com>
2021-05-11 22:52:55 +0800

    add eureka

--- user-service/src/main/java/com/bench/practice/user/feign/DepartmentClient.java
+++ user-service/src/main/java/com/bench/practice/user/feign/DepartmentClient.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "department-client", url = "localhost:8080/api/v1/department")
+@FeignClient(name = "http://DEPARTMENT-SERVICE/api/v1/department")
 public interface DepartmentClient {
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
--- user-service/src/main/java/com/bench/practice/user/service/UserService.java
+++ user-service/src/main/java/com/bench/practice/user/service/UserService.java
@@ -5,11 +5,13 @@ import com.bench.practice.user.feign.DepartmentClient;
 import com.bench.practice.user.repository.UserRepository;
 import com.bench.practice.user.vo.Department;
 import com.bench.practice.user.vo.ResponseTemplateVO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class UserService {
 
@@ -20,10 +22,12 @@ public class UserService {
     private DepartmentClient departmentClient;
 
     public User save(User user) {
+        log.info("Inside saving user");
         return userRepository.save(user);
     }
 
     public ResponseTemplateVO getUserWithDepartment(Long userId) {
+        log.info("Insave getting user with department");
         User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("User id: " + userId + " not existing"));
         ResponseEntity<Department> departmentResponseEntity = departmentClient.findById(user.getDepartmentId());
         Department department = null;
