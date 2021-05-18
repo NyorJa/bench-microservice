c7ed4b8a7f1370cf34e3ac2cd803b00cfde23c2b
Thirdy Fetalvero <botsot.felix@gmail.com>
2021-05-18 17:44:57 +0800

    Remove unused import

--- department-service/src/main/java/com/bench/practice/department/controller/DepartmentController.java
+++ department-service/src/main/java/com/bench/practice/department/controller/DepartmentController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
-
 @RestController
 @RequestMapping(value = "/api/v1/department/")
 @Slf4j
