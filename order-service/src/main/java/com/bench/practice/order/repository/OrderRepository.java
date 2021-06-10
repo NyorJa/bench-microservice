3ae2a82ffb1c4f062346418108b7cee4d14d2ce8
Thirdy Fetalvero <botsot.felix@gmail.com>
2021-06-10 20:37:32 +0800

    Fork main proj

--- order-service/src/main/java/com/bench/practice/order/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bench.practice.order.repository;
-
-import com.bench.practice.order.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- product-service/src/main/java/com/bench/practice/product/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bench.practice.product.repository;
-
-import com.bench.practice.product.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
