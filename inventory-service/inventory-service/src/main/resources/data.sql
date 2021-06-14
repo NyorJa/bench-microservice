21efca1bb53838ccd2eabc91c05ddb592ca61c29
Thirdy Fetalvero <botsot.felix@gmail.com>
2021-06-14 20:51:53 +0800

    Dockerize inventory

--- inventory-service/inventory-service/src/main/resources/data.sql
+++ inventory-service/inventory-service/src/main/resources/data.sql
@@ -1,2 +1,4 @@
-INSERT INTO `inventory-service`.`inventory` (`id`, `sku_code`, `stock`) VALUES ('1', 'IPHONE_12_RED', '100');
-INSERT INTO `inventory-service`.`inventory` (`id`, `sku_code`, `stock`) VALUES ('2', 'IPHONE_12_GREY', '0');
+truncate inventory_service.inventory;
+
+INSERT INTO `inventory_service`.`inventory` (`id`, `sku_code`, `stock`) VALUES ('1', 'IPHONE_12_RED', '100');
+INSERT INTO `inventory_service`.`inventory` (`id`, `sku_code`, `stock`) VALUES ('2', 'IPHONE_12_GREY', '0');
